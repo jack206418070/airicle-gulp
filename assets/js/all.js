e8971d3a70112756f4248359f80be0c0d59008a6
張家祥 <jack90928@gmail.com>
2021-08-09 23:23:48 +0800

    Update 2021-08-09T15:23:45.188Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -7,15 +7,12 @@ function cartHandler(e) {
   e.preventDefault();
   e.stopPropagation();
   var cart = document.querySelector('.cart');
-  var cartFooter = document.querySelector('.cart__footer');
 
   if (e.target.dataset.cart === 'open' || e.target.nodeName === 'span') {
     cart.classList.add('show');
-    cartFooter.classList.add('show');
     e.target.dataset.cart = 'close';
   } else if (e.target.dataset.cart === 'close' || e.target.nodeName === 'span') {
     cart.classList.remove('show');
-    cartFooter.classList.remove('show');
     e.target.dataset.cart = 'open';
   }
 }
