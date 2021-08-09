79292845d55673eb43ee4de1a7c4bc876c07960d
張家祥 <jack90928@gmail.com>
2021-08-09 20:11:44 +0800

    Update 2021-08-09T12:11:41.582Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,22 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
-});
+var cartBtn = document.querySelector('.js-cart');
+cartBtn.addEventListener('click', cartHandler);
+
+function cartHandler(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  var cart = document.querySelector('.cart');
+  var cartFooter = document.querySelector('.cart__footer');
+
+  if (e.target.dataset.cart === 'open' || e.target.nodeName === 'span') {
+    cart.classList.add('show');
+    cartFooter.classList.add('show');
+    e.target.dataset.cart = 'close';
+  } else if (e.target.dataset.cart === 'close' || e.target.nodeName === 'span') {
+    cart.classList.remove('show');
+    cartFooter.classList.remove('show');
+    e.target.dataset.cart = 'open';
+  }
+}
 //# sourceMappingURL=all.js.map
