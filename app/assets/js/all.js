085449beae0b190b1079f4d27847c534bcf53e68
張家祥 <jack90928@gmail.com>
2021-08-09 20:12:05 +0800

    update ticket and pay-1

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,18 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+const cartBtn = document.querySelector('.js-cart');
+cartBtn.addEventListener('click',cartHandler);
+
+function cartHandler(e){
+  e.preventDefault();
+  e.stopPropagation();
+  const cart = document.querySelector('.cart');
+  const cartFooter = document.querySelector('.cart__footer');
+  if(e.target.dataset.cart === 'open' || e.target.nodeName === 'span'){
+    cart.classList.add('show');
+    cartFooter.classList.add('show');
+    e.target.dataset.cart = 'close';
+  }else if(e.target.dataset.cart === 'close' || e.target.nodeName === 'span'){
+    cart.classList.remove('show');
+    cartFooter.classList.remove('show');
+    e.target.dataset.cart = 'open';
+  }
+}
